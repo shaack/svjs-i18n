f5fadb4f981240d5f09c20fb76cfd93f013a90e8
Stefan Haack <shaack@gmail.com>
2018-06-26 19:32:42 +0200

    initial commit

---
+++ src/svjs-i18n/I18n.js
@@ -0,0 +1,24 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/svja-i18n
+ * License: MIT, see file 'LICENSE'
+ */
+
+export class I18n {
+
+    constructor(props = {}) {
+        this.props = {
+            locale: null
+        }
+        Object.assign(this.props, props)
+    }
+
+    load(translations) {
+
+    }
+
+    t(code, ...variables) {
+
+    }
+
+}
